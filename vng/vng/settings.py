40540f6b6fd67e73a53a4baa006069f9d9b93d5c
devahsanumair <ahsan.umair@dynalytix.ai>
2022-11-10 09:37:44 +0000

    Revert "modify license plate api, ec2, database"
    This reverts commit 3cd5fd05dae89107311c10951bffa6e93bf1966e.

--- vng/vng/settings.py
+++ vng/vng/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'vng.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'license_plate_company_data',
+       'USER': 'postgres',
+       'PASSWORD': 'ahsumr',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
 
 
