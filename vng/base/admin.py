610253d97dffcfe7fbe1ea9dc351410d78e0355f
devahsanumair <ahsan.umair@dynalytix.ai>
2022-11-10 09:32:24 +0000

    Revert "modify license plate api, ec2, database"
    This reverts commit 3cd5fd05dae89107311c10951bffa6e93bf1966e.

--- vng/base/admin.py
+++ vng/base/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import LicensePlateCompanyData
+
 # Register your models here.
-admin.site.register(LicensePlateCompanyData)
--- vng/vng/settings.py
+++ vng/vng/settings.py
@@ -77,22 +77,13 @@ WSGI_APPLICATION = 'vng.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'license_plate_company_data',
-       'USER': 'postgres',
-       'PASSWORD': 'ahsumr',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
