a5f7677c28c5493ffd226b0314ab1e643ca0b669
devahsanumair <ahsan.umair@dynalytix.ai>
2022-12-16 12:43:16 +0500

    IndexError Exception

--- vng/base/data/download_rdw_s3.py
+++ vng/base/data/download_rdw_s3.py
@@ -2,6 +2,7 @@ from base import s3_config
 import boto3
 
 def downloadRdwJson(file_name):
+    print(file_name)
     s3 = boto3.client(
         's3', aws_access_key_id=s3_config.config["ACCESS_KEY"],
         aws_secret_access_key=s3_config.config["SECRET_KEY"])
--- vng/base/scheduler/scrap_scheduler.py
+++ vng/base/scheduler/scrap_scheduler.py
@@ -21,26 +21,28 @@ def job():
             image_file = open(image_name, 'rb')
             image_bytes = image_file.read()
             # GET IMAGE SCRAPED DATA
-            license_plate_company_data = fetch_image_detail.company_license_data(
-                image_bytes)
-            # STORE COMPANY DETAILS IN DATABASE
-            if license_plate_company_data['error'] == 'false':
-                company_instance_id = company_database.store_company(
-                    lng, lat, image, license_plate_company_data)
-            os.remove(image_name)
-            # STORE LICENSE NUMBERS IN DATABASE
+            try:
+                license_plate_company_data = fetch_image_detail.company_license_data(
+                    image_bytes)
+                # STORE COMPANY DETAILS IN DATABASE
+                if license_plate_company_data['error'] == 'false':
+                    company_instance_id = company_database.store_company(
+                        lng, lat, image, license_plate_company_data)
+                os.remove(image_name)
+                # STORE LICENSE NUMBERS IN DATABASE
+                if len(license_plate_company_data['license_number']) > 0:
+                    for license_number in license_plate_company_data['license_number']:
+                        license = license_plate_database.store_license_plate(
+                            license_number, image, lng, lat)
+                        # SCRAP RDW DATA ON EACH LICENSE NUMBER
+                        rdw_response = rdw_scrapper.rdw_scrapper(license_number)
+                history.isProcessed = True
+                image.isScraped = True
+                history.save()
+                image.save()
+            except:
+                pass
             
-            if len(license_plate_company_data['license_number']) > 0:
-                for license_number in license_plate_company_data['license_number']:
-                    license = license_plate_database.store_license_plate(
-                        license_number, image, lng, lat)
-                    # SCRAP RDW DATA ON EACH LICENSE NUMBER
-                    rdw_response = rdw_scrapper.rdw_scrapper(license_number)
-                    
-            history.isProcessed = True
-            image.isScraped = True
-            history.save()
-            image.save()
             
             
 def start():
